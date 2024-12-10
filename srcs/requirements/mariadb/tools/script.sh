c05ffc8cf945590dd54703da8d65927d97a0fb11
Myryk <myryk@posteo.de>
2024-12-10 17:17:21 +0100

    added wordpress config

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,9 +4,9 @@
 #db_pwd = User Password
 
 echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
+echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pass' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_pw' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_pass' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
---
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -0,0 +1,29 @@
+sleep 8
+
+if [ ! -f "./wp-config.php" ]; then
+	wp config create	--allow-root \
+						--dbname=$db_name \
+						--dbuser=$db_user \
+						--dbpass=$db_pass \
+						--dbhost=mariadb:3306 \
+						--path='/var/www/wordpress'
+
+	wp core install		--allow-root \
+						--url=$wp_domain \
+						--title=$wp_title \
+						--admin_user=$wp_admin \
+						--admin_password=$wp_admin_pass \
+						--admin_email=$wp_admin_email \
+						--skip-email
+
+	wp user create		--allow-root \
+						$wp_user $wp_email \
+						--role=author \
+						--user_pass=$WP_PWD
+fi
+
+if  [ ! -d "/run/php" ]; then
+	mkdir -p /run/php
+fi
+
+php-fpm8.3 -F
