bef04b685e80c322a79dae8dd2738c69ee5d5e96
Myryk <myryk@posteo.de>
2024-12-17 14:45:16 +0100

    maybe fixing wp

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -5,12 +5,12 @@ cd /var/www/html
 curl -O https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz --strip-components=1
 if [ ! -f "./wp-config.php" ]; then
-	wp config create	--allow-root \
-						--dbname=$db_name \
-						--dbuser=$db_user \
-						--dbpass=$db_pass \
-						--dbhost=mariadb:3306 \
-						--path='/var/www/html'
+	wp config create --allow-root \
+	--dbname=$db_name \
+	--dbuser=$db_user \
+	--dbpass=$db_pass \
+	--dbhost=mariadb:3306 \
+	--path='/var/www/html'
 
 	# wp core install		--allow-root \
 	# 					--url=$wp_domain \
@@ -21,17 +21,17 @@ if [ ! -f "./wp-config.php" ]; then
 	# 					--skip-email
 
 	wp core install --allow-root \
-					--url="$wp_domain" \
-					--title="$wp_title" \
-					--admin_user="$wp_admin" \
-					--admin_password="$wp_admin_pass" \
-					--admin_email="$wp_admin_email" \
-					--skip-email
+	--url="$wp_domain" \
+	--title="$wp_title" \
+	--admin_user="$wp_admin" \
+	--admin_password="$wp_admin_pass" \
+	--admin_email="$wp_admin_email" \
+	--skip-email
 
-	wp user create		--allow-root \
-						$wp_user $wp_email \
-						--role=author \
-						--user_pass=$wp_pwd
+	wp user create --allow-root \
+	$wp_user $wp_email \
+	--role=author \
+	--user_pass=$wp_pwd
 fi
 
 if  [ ! -d "/run/php" ]; then
