59bf9e37a1899bf32d98782aad60e6a3bdaa8094
Myryk <myryk@posteo.de>
2024-12-17 14:36:46 +0100

    wordpress timeout increats

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -4,28 +4,34 @@ sleep 20
 cd /var/www/html
 curl -O https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz --strip-components=1
-if [ ! -f "./wp-config.php" ]; then
-	wp config create	--allow-root \
-						--dbname=$db_name \
-						--dbuser=$db_user \
-						--dbpass=$db_pass \
-						--dbhost=mariadb:3306 \
-						--path='/var/www/html'
+# if [ ! -f "./wp-config.php" ]; then
+# 	wp config create	--allow-root \
+# 						--dbname=$db_name \
+# 						--dbuser=$db_user \
+# 						--dbpass=$db_pass \
+# 						--dbhost=mariadb:3306 \
+# 						--path='/var/www/html'
 
-	wp core install		--allow-root \
-						--url=$wp_domain \
-						--title=$wp_title \
-						--admin_user=$wp_admin \
-						--admin_password=$wp_admin_pass \
-						--admin_email=$wp_admin_email \
-						--skip-email
+# 	wp core install		--allow-root \
+# 						--url=$wp_domain \
+# 						--title=$wp_title \
+# 						--admin_user=$wp_admin \
+# 						--admin_password=$wp_admin_pass \
+# 						--admin_email=$wp_admin_email \
+# 						--skip-email
 
-	wp user create		--allow-root \
-						$wp_user $wp_email \
-						--role=author \
-						--user_pass=$wp_pwd
+# 	wp user create		--allow-root \
+# 						$wp_user $wp_email \
+# 						--role=author \
+# 						--user_pass=$wp_pwd
+# fi
+if  [ ! -f "./wp-config.php" ]; then
+
+wp config create --allow-root   --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/html'
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+
 fi
-
 if  [ ! -d "/run/php" ]; then
 	mkdir -p /run/php
 fi
